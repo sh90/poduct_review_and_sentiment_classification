19277172818c927d75f87b020420fe70c27b8ddc
sh90 <shikhatyagi1990@gmail.com>
2025-08-08 22:03:49 +0530

    Update summarizer.py

--- summarizer.py
+++ summarizer.py
@@ -1,17 +1,17 @@
 import openai
 import os
 
-api_key = os.getenv("OPENAI_API_KEY")
 def summarize_reviews(reviews):
-    openai.api_key = api_key
-    prompt = f"Summarize these product reviews in 2-3 sentences:\n" + "\n".join(reviews)
+    openai.api_key  = os.getenv("OPENAI_API_KEY")
+    prompt = f"Summarize these product reviews in 2-3 crisp bullet points :\n" + "\n".join(reviews)
+    print("summarization prompt and reviews", prompt)
     response = openai.chat.completions.create(
-        model="gpt-4o",
+        model="gpt-4o-mini",
         messages=[
             {"role": "system", "content": "You are a helpful assistant."},
             {"role": "user", "content": prompt}
         ],
-        max_tokens=100,
+        max_tokens=500,
         temperature=0.5
     )
     return response.choices[0].message.content.strip()
