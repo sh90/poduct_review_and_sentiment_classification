0b6d255400ca8c147c756f30a2fe47b6f8f28717
sh90 <shikhatyagi1990@gmail.com>
2025-08-08 12:23:36 +0530

    Update persistence.py

--- persistence.py
+++ persistence.py
@@ -1,19 +1,11 @@
-# persistence.py
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import streamlit as st
 
-import json
-import os
-
-USER_DB_FILE = "user_db.json"
-
-def save_user_db(user_db):
-    """Save the user_db dictionary to a JSON file."""
-    with open(USER_DB_FILE, "w") as f:
-        json.dump(user_db, f, indent=2)
-
-def load_user_db():
-    """Load user_db dictionary from a JSON file. Returns an empty dict if file not found."""
-    if os.path.exists(USER_DB_FILE):
-        with open(USER_DB_FILE, "r") as f:
-            return json.load(f)
-    else:
-        return {}
+def plot_wordcloud(reviews):
+    text = " ".join(reviews)
+    wc = WordCloud(width=800, height=400, background_color='white').generate(text)
+    fig, ax = plt.subplots(figsize=(8, 4))
+    ax.imshow(wc, interpolation='bilinear')
+    ax.axis('off')
+    st.pyplot(fig)
